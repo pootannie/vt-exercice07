36850bd157335672f7d6d47fc09dd54634113be0
Annie Vu <annievu9@gmail.com>
2017-03-01 00:00:08 -0500

    ajout d'un tableau

--- serveur-local.js
+++ serveur-local.js
@@ -1,14 +1,24 @@
 var http = require('http');
 var fs = require('fs');
 
-http.createServer(function (request, response) {  
+http.createServer(function (request, response) {
 	fs.readFile('fichier.json', function (err, data) {
 		if (err) {
 			return console.error(err)
 		}else{
-			response.write(data.toString());
-		};
-		
+			var tableauAffiche = "<table border=1>";
+
+			for(var i=0; i<data.length; i++) {
+				tableauAffiche += "<tr>";
+				tableauAffiche += "<td>" + data[i] + "</td>"
+				tableauAffiche += "</tr>";
+			}
+			tableauAffiche += "</table>";
+
+			response.write(tableauAffiche)
 		response.end();
-	});
+		}
+	})
 }).listen(8081);
+
+var data = fs.readFileSync('fichier.json');
